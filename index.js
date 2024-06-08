90f01cc219d9c1df523a14b9f05f8af607c69d61
devjangid2005 <jangiddev564@gmail.com>
2024-06-08 11:48:18 +0530

    basic of javascript

---
+++ index.js
@@ -0,0 +1,93 @@
+//var
+
+// var num = 10;
+// {
+
+//     num = 20;
+//     console.log(num)
+
+// }
+// console.log("var",num)
+
+// //let
+// let num1 = 10;
+// {
+
+//     num1 = 20;
+//     console.log(num1)
+// }
+// console.log("let",num1)
+
+// //const
+// const num2 = 10;
+// {
+
+//   const num2 = 20;
+//   console.log(num2)
+
+// }
+// console.log(num2)
+// //sum
+// let num1=10;
+// let num2=20;
+// let sum=num1+num2;
+// console.log(sum)
+
+// //sub
+// let num3=20;
+// let num4=10;
+// let sub=num3-num4;
+// console.log(sub)
+
+// //mul
+// let num5=10;
+// let num6=5;
+// let mul=num5*num6;
+// console.log(mul)
+
+// //div
+// let num7=10;
+// let num8=5;
+// let div=num7/num8;
+// console.log(div)
+
+// //mode
+// let num9=4;
+// let num10=2;
+// let mode=num9%num10
+// console.log(mode)
+
+// let num1=20
+// let num2=10
+// let sum=num1++ + num1 + num1++
+// console.log(num1)
+// console.log(sum)
+
+// let num1=5;
+// let num2=10;
+
+// let sum=--num1+ ++num2 + num1-- +num2++
+// console.log(sum)
+// console.log(num1)
+// console.log(num2)
+
+// let checkIn=10;
+// let checkOut="10";
+// let obj={name:"dev"}
+// let arr=[4,4];
+
+// console.log(!checkIn)
+// console.log(typeof checkIn)
+// console.log(typeof checkOut)
+// console.log(typeof obj)
+// console.log(typeof arr)
+// console.log(checkIn==checkOut)
+// console.log(checkIn===checkOut)
+// let a=10;
+// let b=20;
+// let c=30;
+// let result=(a>b && a>c)?"a":(b>c)?"b":"c";
+// console.log(result);
+let age=16;
+let vote=(age>18)?"yes you can vote":"you must be 18+"
+console.log(vote);
